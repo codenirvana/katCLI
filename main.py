8b0bfe809546185e03fc277c29c512b0414dc98e
Udit <admin@codenirvana.in>
2016-01-15 22:14:55 +0530

    clean up

--- main.py
+++ main.py
@@ -1,18 +1,4 @@
-import requests, bs4, click, __future__
-
-base_link = 'https://kat.cr/usearch/'
-
-field_filter = {
-    'size'  : 'size',
-    'files' : 'files_count',
-    'age'   : 'time_add',
-    'seed'  : 'seeders',
-    'leech' : 'leechers'
-}
-
-sorder_filter = ('asc', 'desc')
-
-category_filter = ['all', 'movies', 'tv', 'anime', 'music', 'books', 'applications', 'xxx']
+import click, __future__
 
 def colors():
   enums = dict(
@@ -27,36 +13,6 @@ def colors():
 
 def cap(s, l):
     return s if len(s)<=l else s[0:l-3]+'...'
-
-
-def get_data(**args):
-    search = args.get('search', '')
-    category = args.get('category', 'all')
-    field = args.get('field', 'age')
-    sorder = args.get('sorder', 'desc')
-    page = args.get('page', '1')
-
-    dict_keys = ['name', 'link', 'category', 'size', 'files', 'age', 'seed', 'leech']
-
-    search_query = search + ' category:' + category
-    link = base_link + search_query + '/' + page + '/?field=' + field_filter[field] +'&sorder=' + sorder
-
-    response = requests.get(link)
-    soup = bs4.BeautifulSoup(response.text,"html.parser")
-    rows = soup.select('[id^=torrent_]')
-
-    data = []
-    for row in rows:
-        cols = row.find_all('td')
-        name = cols[0].select('.cellMainLink')
-        link = cols[0].select('.iaconbox a')
-        category = cols[0].select('[id^=cat_]')
-        row_data = [ name[0].text, 'http:' + link[-1].get('href'), category[0].text]
-        for i in range(1,6):
-            row_data.append(cols[i].text.strip())
-        data.append( dict(zip( dict_keys, list( (x.replace(u'\xa0', u' ')).encode('ascii', 'ignore').decode("utf-8", "ignore") in row_data) )) )
-
-    return data
 
 def print_data(data):
     count = 0
